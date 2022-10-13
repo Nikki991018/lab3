98c31afa2d9233f6e7aeb6372bd3b9982d197894
Nikki991018 <qil026@ucsd.edu>
2022-10-13 13:09:33 -0700

    lab3

--- ArrayTests.java
+++ ArrayTests.java
@@ -28,4 +28,10 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
+  }
 }
